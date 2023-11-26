fb14635de461a76749e853022ecf1be916d3c4f8
The Turing Taco Tales <tales@turingtaco.com>
2023-11-26 20:42:43 +0100

    bugfix: Fix error when accessing the token

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from datetime import timedelta
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models.models import Base
+from models.models import Base, User
 from models.db_init import init_db
+from models.auth import authenticate_user, create_access_token, get_current_user, ACCESS_TOKEN_EXPIRE_MINUTES
+
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(
@@ -20,3 +24,23 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Welcome to TuringTaqueriaApi!"}
+
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    with SessionLocal() as db:
+        user = authenticate_user(db, form_data.username, form_data.password)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(
+            data={"sub": user.username}, expires_delta=access_token_expires
+        )
+        return {"access_token": access_token, "token_type": "bearer"}
+
+@app.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
